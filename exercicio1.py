8fd1255d2506ad5ca16b43d2d94c5d61d071398d
JoaoVictorMontagna <Joao.Montagna@sou.inteli.edu.br>
2023-02-09 09:41:05 -0300

    Ex

--- exercicio1.py
+++ exercicio1.py
@@ -12,7 +12,37 @@
 
 
 
+
 # Teste a sua função aqui (caso ache necessário)
+text=input("Digite um texto para contarmos as palavras unicas")
+
+c=[",",".","?","!"]
+
+for i in c:
+    remover_pontos=text.replace(i,"")
+    text=remover_pontos
+
+palavras=text.split(" ")
+palavrasUnicas=set(palavras)
+qntPalavras=len(palavrasUnicas)
+print(qntPalavras)
+
+    
+
+
+
+
+
+
+    
+
+
+
+
+
+    
+    
+
 
 
 
--- exercicio2.py
+++ exercicio2.py
@@ -13,6 +13,19 @@
 
 
 # Teste a sua função aqui (caso ache necessário)
+string1="PEDRA"
+string2="ARDEP"
+# def is_anagram(string1, string2):
+string1 = string1.lower()
+string2 = string2.lower()
+if sorted(string1) == sorted(string2):
+    print("É um anagrama")
+else:
+    print("Não é um anagrama")
+        
+
+
+
 
 
 
