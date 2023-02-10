7bc8f1105b5879e7748de26ce15ff9812f686b71
JoaoVictorMontagna <Joao.Montagna@sou.inteli.edu.br>
2023-02-10 09:13:10 -0300

    funçoes

--- exercicio2.py
+++ exercicio2.py
@@ -13,15 +13,25 @@
 
 
 # Teste a sua função aqui (caso ache necessário)
-string1="PEDRA"
-string2="ARDEP"
+
+
 # def is_anagram(string1, string2):
-string1 = string1.lower()
-string2 = string2.lower()
-if sorted(string1) == sorted(string2):
-    print("É um anagrama")
-else:
-    print("Não é um anagrama")
+p1=input("Digite a primeira palavra a ser comparada")
+
+p2=input("Digite a segunda palavra a ser comparada")
+
+def is_anagram(string1, string2):
+    string1 = string1.lower()
+    string2 = string2.lower()
+    if sorted(string1) == sorted(string2):
+        print(p1 + " e " + p2 +" são um anagrama")
+
+    else:
+        print(p1 + " e " + p2 +" não são um anagrama")
+
+print(is_anagram(p1,p2))
+
+
         
 
 
