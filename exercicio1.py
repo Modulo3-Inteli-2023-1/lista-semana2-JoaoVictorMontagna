b57ef5f974bd364b1b21b6a558e0da8e41e3b06d
JoaoVictorMontagna <Joao.Montagna@sou.inteli.edu.br>
2023-02-16 14:12:34 -0300

    Feito

--- exercicio1.py
+++ exercicio1.py
@@ -10,25 +10,25 @@
 
 
 
+def conta_palavras_unicas(text):
+   palavras= text.split(" ")
+   palavrasUnicas=set(palavras)
+   qntPalavras=len(palavrasUnicas)
+   return qntPalavras
+
+
+
+
+
+
 
 
 
 # Teste a sua função aqui (caso ache necessário)
-frase=input("Digite um texto para contarmos as palavras unicas")
 
-pontos=[",",".","?","!"]
 
-def conta_palavras_unicas(text,c:list):
-    for i in c:
-        remover_pontos=text.replace(i,"")
-        text=remover_pontos
 
-    palavras=text.split(" ")
-    palavrasUnicas=set(palavras)
-    qntPalavras=len(palavrasUnicas)
-    print(qntPalavras)
 
-print(conta_palavras_unicas(frase,pontos))
 
     
 
--- exercicio2.py
+++ exercicio2.py
@@ -16,20 +16,19 @@
 
 
 # def is_anagram(string1, string2):
-p1=input("Digite a primeira palavra a ser comparada")
 
-p2=input("Digite a segunda palavra a ser comparada")
 
 def is_anagram(string1, string2):
     string1 = string1.lower()
     string2 = string2.lower()
+
     if sorted(string1) == sorted(string2):
-        print(p1 + " e " + p2 +" são um anagrama")
+        return True
 
     else:
-        print(p1 + " e " + p2 +" não são um anagrama")
+        return False
 
-print(is_anagram(p1,p2))
+
 
 
         
